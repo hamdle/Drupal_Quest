e14b66e9c36b1dec42441d54a92a5ff0ef9bba71
hamdle <contact@ericmarty.co>
2018-05-27 13:01:52 -0700

    Base player movement

--- Character.gd
+++ Character.gd
@@ -4,9 +4,6 @@ extends KinematicBody2D
 # Physics and Jump
 const UP = Vector2(0, -1)
 const GRAVITY = 10
-const ACCELERATION = 20
-const MAX_SPEED = 200
-const JUMP_HEIGHT = 300
 
 var motion = Vector2()
 
@@ -14,4 +11,5 @@ func _physics_process(delta):
 	#Gravity
 	motion.y += GRAVITY
 	
+	# Process movement using Godot physics system
 	motion = move_and_slide(motion, UP)
--- Player.gd
+++ Player.gd
@@ -1,12 +1,22 @@
 extends "res://Character.gd"
 
+const ACCELERATION = 20
+const MAX_SPEED = 200
+const JUMP_HEIGHT = 300
+const FLOOR_FRICTION = 0.2
 	
 func _physics_process(delta):
+	var friction = false
+	
 	# Controls
 	if Input.is_action_pressed("move_right"):
 		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$Position2D/Sprite.flip_h = false
-	if Input.is_action_pressed("move_left"):
+	elif Input.is_action_pressed("move_left"):
 		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Position2D/Sprite.flip_h = true
-		
+	else:
+		friction = true
+	
+	if friction:
+		motion.x = lerp(motion.x, 0, FLOOR_FRICTION)
