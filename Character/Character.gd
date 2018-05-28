58b19a20ab9fff8ba83721b7f21b7af7978b9882
hamdle <contact@ericmarty.co>
2018-05-27 21:17:15 -0700

    Add HIT state

--- Character/Character.gd
+++ Character/Character.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	$AnimationPlayer.play("FLOAT")
+	# $AnimationPlayer.play("FLOAT")
 	pass # replace with function body
--- Character/Player/Player.gd
+++ Character/Player/Player.gd
@@ -5,12 +5,13 @@ var motion = Vector2()
 
 # State Machine
 var current_state
-enum STATE { NULL, IDLE, WALK, JUMP }
+enum STATE { NULL, IDLE, WALK, JUMP, HIT }
 
 onready var state_nodes = {
 	STATE.IDLE: $States/Idle,
 	STATE.WALK: $States/Walk,
-	STATE.JUMP: $States/Jump
+	STATE.JUMP: $States/Jump,
+	STATE.HIT: $States/Hit
 }
 
 func _ready():
@@ -34,6 +35,9 @@ func _input(event):
 	# Debug
 	if Input.is_action_just_pressed("fire"):
 		$AnimationPlayer.play("HIT")
+		current_state.exit()
+		current_state = state_nodes[STATE.HIT]
+		current_state.enter(self)
 	
 	# Process state machine
 	var new_state = current_state.handleInput(event)
@@ -44,3 +48,9 @@ func _input(event):
 	
 func flip_sprite(flip):
 	$Position2D/Sprite.flip_h = flip
+	
+func _on_AnimationPlayer_animation_finished(anim_name):
+	$AnimationPlayer.play("FLOAT")
+	current_state.exit()
+	current_state = state_nodes[STATE.IDLE]
+	current_state.enter(self)
