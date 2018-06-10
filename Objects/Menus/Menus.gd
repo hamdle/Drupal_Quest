b581178bed1c93faed4d3e4d62d2b4cdd71d5811
hamdle <contact@ericmarty.co>
2018-06-09 22:41:18 -0700

    Wire up Menus and fix pause timer issue

--- Objects/Menus/Menus.gd
+++ Objects/Menus/Menus.gd
@@ -1,6 +1,9 @@
 extends Node
 
 var start_time
+var pause_time
+var paused = false
+var showing_menu = false
 
 export var current_level = "none"
 export var next_level = "none"
@@ -14,8 +17,9 @@ func _ready():
 
 func _input(event):
 	# Escape to Start screen shortcut
-	if Input.is_action_just_pressed("escape"):
-		level_pause()
+	if not showing_menu:
+		if Input.is_action_just_pressed("escape"):
+			_level_pause()
 
 func player_died():
 	$DieMenu.visible = true
@@ -23,18 +27,45 @@ func player_died():
 	
 func player_win():
 	$WinMenu.visible = true
+	
 	# Set finish time
 	var finish_time = (OS.get_ticks_msec() - start_time) / 1000.0
 	var time_label = $WinMenu/CC/VBC/MC3/TimeLabel
 	time_label.text = String(finish_time)
 	_level_over($WinMenu)
 
-func level_pause():
-	$PauseMenu.visible = true
+func _level_pause():
+	paused = not paused
+	
+	if paused == false:
+		print(start_time)
+		print(pause_time)
+		print(OS.get_ticks_msec() - pause_time)
+		start_time = start_time + (OS.get_ticks_msec() - pause_time)
+	
+	pause_time = OS.get_ticks_msec()
+	
+	# Show current time
+	var current_time = (pause_time - start_time) / 1000.0
+	var time_label = $PauseMenu/CC/VBC/MC3/TimeLabel
+	time_label.text = String(current_time)
+	# Set menu location
+	var player = get_tree().get_root().get_node("World/Player")
+	var new_loc = Vector2()
+	new_loc.x = player.position.x
+	new_loc.y = player.position.y - 50
+	$PauseMenu.position = new_loc
+	
+	$PauseMenu.visible = paused
+	get_tree().paused = paused
+	
+	
 
 func _level_over(menu):
 	
 	get_tree().paused = true
+	
+	showing_menu = true
 	
 	# Set menu location
 	var player = get_tree().get_root().get_node("World/Player")
