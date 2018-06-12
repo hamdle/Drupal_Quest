c30eba223a8847c7fe9129325aebb1eaa9944db4
hamdle <contact@ericmarty.co>
2018-06-11 23:39:58 -0700

    First attempt at wait timer

--- Cutscene/States/wait.gd
+++ Cutscene/States/wait.gd
@@ -2,19 +2,26 @@ extends Node
 
 enum STATE { NULL, IDLE, SHOW_TEXT, WAIT }
 
-var output_text
+var timer_done = false
 
 func enter(processor):
 	print(processor.name + " wait")
 
+	var timer = processor.get_node("Timer")
+	timer.start()
 	pass
 
 func exit(processor):
 	pass
 	
 func update(processor, delta):
-	return processor.process_next_index()
+	if timer_done:
+		return processor.process_next_index()
 	pass
 	
 func handleInput(processor, event):
 	pass
+
+
+func _on_Timer_timeout():
+	timer_done = true
