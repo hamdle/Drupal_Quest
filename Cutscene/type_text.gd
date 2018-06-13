31afc6637fc420c8a5bf70d9e996f07b101ebe87
hamdle <contact@ericmarty.co>
2018-06-12 23:15:54 -0700

    Implement type_text

--- Cutscene/type_text.gd
+++ Cutscene/type_text.gd
@@ -2,6 +2,9 @@ extends Node
 
 var text_to_type
 var type_speed
+var index
+
+var complete = false
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -11,9 +14,25 @@ func _ready():
 func start(arg1, arg2):
 	text_to_type = arg1
 	type_speed = arg2
+	$Timer.wait_time = float(type_speed)
+	print($Timer.wait_time)
+	$Timer.start()
+	index = 0
+	complete = false
 	pass
 
 func run(processor):
-	$Container/Label.text = text_to_type
-	processor.get_next = true
+	if complete == true:
+		$Timer.stop()
+		processor.get_next = true
 	pass
+
+func type_next_letter():
+	index += 1
+	if index <= text_to_type.length():
+		$Container/Label.text = text_to_type.substr(0, index)
+	else:
+		complete = true
+
+func _on_Timer_timeout():
+	type_next_letter()
