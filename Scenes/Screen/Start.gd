1f43a4b96672aa7e9df4fc1d8ae1d3c212987984
hamdle <contact@ericmarty.co>
2018-11-04 20:52:29 -0800

    Redo buttons and level select page layout

--- Scenes/Screen/Start.gd
+++ Scenes/Screen/Start.gd
@@ -11,3 +11,9 @@ func _on_ArcadeButton_pressed():
 	var gs = get_node("/root/gamestate")
 	gs.arcade_mode()
 	gs.load_scene(gs.level_map["character"])
+
+
+func _on_ArcadeButton2_pressed():
+	var gs = get_node("/root/gamestate")
+	gs.arcade_mode()
+	gs.load_scene(gs.level_map["character"])
