53decccb4a90d6669b235a126f3eedf18909d01f
hamdle <contact@ericmarty.co>
2018-11-10 16:42:06 -0800

    Build out first three levels

--- Objects/Gates/GateB.gd
+++ Objects/Gates/GateB.gd
@@ -4,6 +4,7 @@ export var gate_player = true
 export var gate_bug = true
 export var gate_flying = true
 export var gate_key = true
+export var spawn_y_diff = 70
 
 func _ready():
 	pass
@@ -26,7 +27,7 @@ func teleport_object_to_gate_a(body):
 	var gate_a = get_tree().get_root().get_node("World/Gates/GateA")
 	# Set player to Gate B position
 	var teleport_to_position = gate_a.position
-	teleport_to_position.y += 50
+	teleport_to_position.y += spawn_y_diff
 	body.position = teleport_to_position
 	body.motion = Vector2(0, 0)
 	print("gate " + body.name + " to a")
